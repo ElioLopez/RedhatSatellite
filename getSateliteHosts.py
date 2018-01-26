fa72c84d722e9e02d733e87b0aa10106145f6e91
ElioLopez <35702545+ElioLopez@users.noreply.github.com>
2018-01-26 13:18:16 -0300

    Add files via upload

---
+++ getSateliteHosts.py
@@ -0,0 +1,28 @@
+#este script trae todos los hosts que son Centos y estan incorrectamente suscriptos al satellite
+!/usr/bin/python
+import xmlrpclib
+
+SATELLITE_URL = "https://satellite.example.com"
+SATELLITE_LOGIN = "foo"
+SATELLITE_PASSWORD = "bar"
+
+client = xmlrpclib.Server(SATELLITE_URL, verbose=0)
+
+key = client.auth.login(SATELLITE_LOGIN, SATELLITE_PASSWORD)
+
+def search(name, dicts, key="name"):
+try:
+return (item for item in dicts if item[key] == name).next()
+except:
+return None
+
+systems = client.system.listSystems(key)
+for system in systems:
+packages = client.system.listPackages(key,system['id'])
+rel = search('redhat-release',packages)
+if not rel:
+rel = search('redhat-release-server',packages)
+if rel:
+print "%s %s" % ( system['name'], rel['release'])
+else:
+print "%s Unknown" % ( system['name'])
